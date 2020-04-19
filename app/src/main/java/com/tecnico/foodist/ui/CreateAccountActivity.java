eaa6f21364bac966e0c0840575514db0700b4ce3
Rodrigo Nobre <rodrigo.nobre@tecnico.ulisboa.pt>
2020-04-19 04:46:48 +0100

    Fully Implemented UserAccounts

--- app/src/main/java/com/tecnico/foodist/ui/CreateAccountActivity.java
+++ app/src/main/java/com/tecnico/foodist/ui/CreateAccountActivity.java
@@ -5,18 +5,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.tecnico.foodist.MainActivity;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.tecnico.foodist.R;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -26,14 +33,18 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
 
     private EditText email;
     private EditText password;
+    private EditText name;
     private FirebaseAuth mAuth;
     private ProgressDialog dialog;
+    private Spinner dropdown;
+    private String status = "";
+    private FirebaseFirestore fstore;
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_create_account);
 
         //setUp Progress Dialog
         dialog = new ProgressDialog(this);
@@ -43,13 +54,37 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
 
-        //Declaration
+        //Button Declaration
         Button button = (Button) findViewById(R.id.create_acount_button);
         button.setOnClickListener(CreateAccountActivity.this);
+
+        //Find Views Declaration
         email   = (EditText)findViewById(R.id.create_email);
         password   = (EditText)findViewById(R.id.create_password);
+        name   = (EditText)findViewById(R.id.Name);
 
-        //Auth setup
+        //setup Spinner
+        dropdown = findViewById(R.id.spinner1);
+        String[] items = new String[]{"Student", "Professor", "Researcher", "Staff", "General Public"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+
+        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                String item = dropdown.getSelectedItem().toString();
+                status = item;
+                Log.i("Selected item:", item);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+            }
+        });
+
+        //instanciate firestore
+        fstore = FirebaseFirestore.getInstance();
+
+        //See if user is already loggedIn
         if (mAuth.getCurrentUser() != null){
             Toast.makeText(CreateAccountActivity.this, "User is currently LogIn", Toast.LENGTH_SHORT).show();
             startActivity(new Intent(getApplicationContext(),LoginActivity.class));
@@ -64,7 +99,11 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
 
         switch (v.getId()){
             case R.id.create_acount_button:{
-                if (email.getText().toString().matches("")){
+
+                if (name.getText().toString().matches("")){
+                    name.setError("Name is required");
+                }
+                else if (email.getText().toString().matches("")){
                     email.setError("Email is required");
                 }
                 else if (password.getText().toString().matches("")){
@@ -73,10 +112,15 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
                 else if (password.getText().toString().length() < 6){
                     password.setError("Password must be >= 6 characters");
                 }
+                else if (status.equals("")){
+                    ((TextView)dropdown.getSelectedView()).setError("Nothing selected");
+                }
                 else{
+                    Log.w("status",status);
                     dialog.show();
                     createAccount(email.getText().toString(), password.getText().toString());
                 }
+
                 break;
             }
         }
@@ -85,6 +129,8 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
     private void createAccount(String email, String password) {
 
         Log.w("email", email);
+
+        //TO DO VERIFICAR SE O EMAIL EXISTE OU NAO
 
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -95,8 +141,27 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
                             Toast.makeText(CreateAccountActivity.this, "User created. Please login LogIn", Toast.LENGTH_SHORT).show();
                             //startActivity(new Intent(getApplicationContext(), MainActivity.class));
                             Log.d("createSuccess", "createUserWithEmail:success");
-                            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
 
+                            String userID = mAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fstore.collection("users").document(userID);
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("Name", name.getText().toString());
+                            user.put("Email", email);
+                            user.put("UniversityStatus", status.toString());
+
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d("userUpload", "User fully uploaded");
+                                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d("userUploadFailed", "Failed");
+
+                                }
+                            });
 
                         } else {
                             // If sign in fails, display a message to the user.
--- app/src/main/java/com/tecnico/foodist/ui/ProfileActivity.java
+++ app/src/main/java/com/tecnico/foodist/ui/ProfileActivity.java
@@ -1,4 +1,5 @@
 package com.tecnico.foodist.ui;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,12 +10,25 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.tecnico.foodist.MainActivity;
 import com.tecnico.foodist.R;
 
 public class ProfileActivity  extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseUser user;
+    TextView name;
+    TextView status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +38,32 @@ public class ProfileActivity  extends AppCompatActivity implements View.OnClickL
         Button logoutButton = findViewById(R.id.logoutButton);
         logoutButton.setOnClickListener(ProfileActivity.this);
 
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        user = FirebaseAuth.getInstance().getCurrentUser();
 
-        TextView email = (TextView)findViewById(R.id.emailText);
+        TextView email = (TextView)findViewById(R.id.email);
+        name = (TextView)findViewById(R.id.name);
+        status = (TextView)findViewById(R.id.status);
+
         email.setText(user.getEmail());
 
+        getUserProperties();
+
+
     }
+
+    private void getUserProperties() {
+        FirebaseFirestore rootRef = FirebaseFirestore.getInstance();
+        DocumentReference userRef = rootRef.collection("users").document(user.getUid());
+
+        userRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                name.setText(documentSnapshot.getString("Name"));
+                status.setText(documentSnapshot.getString("UniversityStatus"));
+            }
+        });
+
+        }
 
     @Override
     public void onClick(View v) {
